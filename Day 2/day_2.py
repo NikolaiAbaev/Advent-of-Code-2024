d7e076e30f82acdda12439b7957f4d8726db79e5
Nikolai Abaev <nikolai.abayeu@gmail.com>
2024-12-07 16:23:50 -0500

    attempt_day_2_part_2

--- Day 2/day_2.py
+++ Day 2/day_2.py
@@ -1,4 +1,7 @@
+#Part I -- Day 2: Red-Nosed Reports
+
 safe_codes = []
+all_codes = []
 
 with open("data.txt", "r") as file: 
     for line in file:
@@ -6,6 +9,9 @@ with open("data.txt", "r") as file:
         temp = []
         for i in strip:
             temp.append(int(i))
+
+        #adding all codes here for part II
+        all_codes.append(temp)
         
         safe = True
         if temp[0] - temp[1] > 0:
@@ -26,4 +32,37 @@ with open("data.txt", "r") as file:
             safe_codes.append(temp)
 
 answer = len(safe_codes)
+print(f'{answer} Codes are Safe')
 
+#Part II 
+safe_codes_part_two = []
+
+for code in all_codes:
+    damper = False 
+    safe = True
+    if code[0] - code[1] > 0:
+        increasing = False
+    else:
+        increasing = True
+
+    for i in range(1, len(code), 1):
+        print(i)
+        if abs(code[i - 1] - code[i]) > 3 or abs(code[i - 1] - code[i]) < 1:
+            if damper == True:
+                damper = False
+                i += 2
+            else:
+                safe = False
+                break
+
+        if (increasing == True and code[i - 1] - code[i] > 0) or (increasing == False and code[i - 1] - code[i] < 0):
+            if damper == True:
+                damper = False
+            else:
+                safe = False
+                break
+
+    if safe == True:
+        safe_codes_part_two.append(code)
+
+print(len(safe_codes_part_two))
