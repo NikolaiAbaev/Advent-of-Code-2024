1db33b4eb3f597e91593a63ec28d3c68bf2e7586
Nikolai Abaev <nikolai.abayeu@gmail.com>
2024-12-07 10:32:55 -0500

    first commit

---
+++ Day 1/Day_1.py
@@ -0,0 +1,20 @@
+left = []
+right = []
+
+distance = 0 
+
+with open("data.txt", "r") as file:
+    for line in file:
+        strip = line.rstrip().split()
+        left.append(int(strip[0]))
+        right.append(int(strip[1]))
+
+left.sort()
+right.sort()
+
+for i in range(len(left)):
+    x = left[i] - right[i]
+    if x < 0:
+        x *= -1 
+    distance += x
+
