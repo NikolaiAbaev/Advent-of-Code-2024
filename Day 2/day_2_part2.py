18a3b7a72f5b684dc9bc41d4b90e4cc65e3cf85c
Nikolai Abaev <nikolai.abayeu@gmail.com>
2024-12-08 18:08:00 -0500

    update

--- Day 2/day_2_part2.py
+++ Day 2/day_2_part2.py
@@ -5,20 +5,21 @@ safe_codes = []
 def is_safe(list) -> bool:
     if len(list) < 2:
         return True
-    
+    left, right = 0, 1
     damper_used = False    
 
-    for i in range(1, len(list)):
-        diff = abs(list[i] - list[i - 1])
+    while right < len(list):
+        diff = abs(list[right] - list[left])
         
         if diff > 3 or diff == 0:
             if damper_used:
                 return False
             damper_used = True
             # [1, 4, 5, 6, 7, 8]
-            if abs(list[i] - list[i + 1]) > 3 or abs(list[i] - list[i + 1]) == 0:
-                 
-            elif abs(list[i - 1] - list[i + 1]):
+            if (abs(list[right] - list[right + 1]) < 4 and abs(list[right] - list[right + 1]) != 0):
+                right += 2
+                left += 2
+            elif abs(list[left] - list[right + 1]):
                 do 
             else:
                 return False 
