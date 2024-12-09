1550f524545073640b00b01cf898c6b2b94f36cb
Nikolai Abaev <nikolai.abayeu@gmail.com>
2024-12-08 22:24:11 -0500

    day2

--- Day 2/day_2_part2.py
+++ Day 2/day_2_part2.py
@@ -1,33 +1,43 @@
-#Part II -- Day 2: Red-Nosed Reports
+#Part II -- Day 2: Red-Nosed Reports (359 - 371)
 safe_codes = []
-
 
 def is_safe(list) -> bool:
     if len(list) < 2:
         return True
+    
     left, right = 0, 1
     damper_used = False    
 
+    increasing = list[left] < list[right] 
+
     while right < len(list):
-        diff = abs(list[right] - list[left])
+        diff = list[right] - list[left]
         
-        if diff > 3 or diff == 0:
+        if abs(diff) > 3 or abs(diff) == 0 or (increasing and diff < 0) or (not increasing and diff > 0):
             if damper_used:
+                print(f'Nope this {list} is not good!')
                 return False
             damper_used = True
-            # [1, 4, 5, 6, 7, 8]
-            if (abs(list[right] - list[right + 1]) < 4 and abs(list[right] - list[right + 1]) != 0):
-                right += 2
-                left += 2
-            elif abs(list[left] - list[right + 1]):
-                do 
+
+            if right < len(list) - 1:
+                if (abs(list[right] - list[right + 1]) < 4 and abs(list[right] - list[right + 1]) != 0):
+                    if (increasing and (list[right] - list[right + 1]) < 0) or (not increasing and (list[right] - list[right + 1]) > 0):
+                        right += 2
+                        left += 2
+                elif abs(list[left] - list[right + 1]) < 4 and abs(list[left] - list[right + 1] != 0):
+                    if (increasing and (list[left] - list[right + 1]) < 0) or (not increasing and (list[left] - list[right + 1]) > 0):    
+                        right += 2
+                        left += 2
+                else:
+                    print(f'Nope this {list} is not good!')
+                    return False
             else:
-                return False 
-
-
+                return True
+        else:
+            left += 1
+            right += 1
     return True
         
-
 
 with open("data.txt", "r") as file: 
     for line in file:
